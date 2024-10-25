593a666fa4398e7e3ade06be5e9a723caed25278
gooplet0075 <edmbeech@gmail.com>
2024-10-25 15:18:32 -0400

    not even gonna try w/ the individualized commit messages b/c i changed way too much

---
+++ goban/goban.js
@@ -0,0 +1,120 @@
+let placeholder = `//a b c d e f g h i j k l m n o p q r s\\
+a ┏━┯━┯━┯━┯━┯━┯━┯━┯━┯━┯━┯━┯━┯━┯━┯━┯━┯━┓ a
+b ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ b
+c ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ c
+d ┠─┼─┼─╋─┼─┼─┼─┼─┼─╋─┼─┼─┼─┼─┼─╋─┼─┼─┨ d
+e ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ e
+f ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ f
+g ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ g
+h ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ h
+i ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ i
+j ┠─┼─┼─╋─┼─┼─┼─┼─┼─╋─┼─┼─┼─┼─┼─╋─┼─┼─┨ j
+k ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ k
+l ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ l
+m ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ m
+n ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ n
+o ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ o
+p ┠─┼─┼─╋─┼─┼─┼─┼─┼─╋─┼─┼─┼─┼─┼─╋─┼─┼─┨ p
+q ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ q
+r ┠─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┨ r
+s ┗━┷━┷━┷━┷━┷━┷━┷━┷━┷━┷━┷━┷━┷━┷━┷━┷━┷━┛ s
+▪▫a b c d e f g h i j k l m n o p q r s//`
+
+class KosumiGoban {
+    constructor(parent) {
+        this.parent = parent;
+
+        this.container = document.createElement('div');
+        this.container.classList.add('gobanContainer');
+        this.parent.appendChild(this.container);
+
+        this.boardState = document.createElement('div');
+        this.boardState.classList.add('gobanBoardState');
+        this.boardState.innerText = placeholder;
+        this.container.appendChild(this.boardState);
+
+        this.navigation = document.createElement('div');
+        this.navigation.classList.add('gobanNavigationPanel');
+        this.container.appendChild(this.navigation);
+
+        this.skipBackwardButton = document.createElement('button');
+        this.stepBackwardButton = document.createElement('button');
+        this.stepForewardButton = document.createElement('button');
+        this.skipForewardButton = document.createElement('button');
+        this.skipBackwardButton.innerHTML = '<i class="fa fa-fast-backward"></i>'
+        this.stepBackwardButton.innerHTML = '<i class="fa fa-step-backward"></i>'
+        this.stepForewardButton.innerHTML = '<i class="fa fa-step-forward"></i>'
+        this.skipForewardButton.innerHTML = '<i class="fa fa-fast-forward"></i>'
+        this.skipBackwardButton.classList.add('gobanNavigationButton');
+        this.stepBackwardButton.classList.add('gobanNavigationButton');
+        this.stepForewardButton.classList.add('gobanNavigationButton');
+        this.skipForewardButton.classList.add('gobanNavigationButton');
+        const object = this;
+        this.skipBackwardButton.addEventListener('click',function() {
+            object.skipBackward()
+        });
+        this.stepBackwardButton.addEventListener('click',function() {
+            object.stepBackward()
+        });
+        this.stepForewardButton.addEventListener('click',function() {
+            object.stepForeward()
+        });
+        this.skipForewardButton.addEventListener('click',function() {
+            object.skipForeward()
+        });
+
+        this.navigation.append(
+            this.skipBackwardButton,
+            this.stepBackwardButton,
+            this.stepForewardButton,
+            this.skipForewardButton
+        )
+
+        this.info = document.createElement('textarea');
+        this.info.classList.add('gobanInfo');
+        this.container.appendChild(this.info);
+
+        this.activeNode;
+        this.gameTree;
+        this.prettify;
+        this.getNodeById;
+        this.getState;
+        this.getLastMainNode;
+    }
+
+    skipBackward() {
+        this.activeNode = this.gameTree;
+        this.boardState.innerText = this.prettify(this.activeNode.state);
+        this.info.value = `(Node: ${this.activeNode.id}) Move ${this.activeNode.moveNumber}:\n${JSON.stringify(this.activeNode.props)}`;
+    }
+
+    stepBackward() {
+        if (this.activeNode.hasOwnProperty('parent')) {
+            this.activeNode = this.getNodeById(this.gameTree,this.activeNode.parent);
+            this.boardState.innerText = this.prettify(this.activeNode.state);
+        }
+        this.info.value = `(Node: ${this.activeNode.id}) Move ${this.activeNode.moveNumber}:\n${JSON.stringify(this.activeNode.props)}`;
+    }
+    
+    stepForeward() {
+        if (this.activeNode.hasOwnProperty('children')) {
+            let newState = this.getState(this.activeNode.state, this.activeNode.children[0].props);
+            this.activeNode = this.activeNode.children[0];
+            this.activeNode.state = newState;
+            this.boardState.innerText = this.prettify(newState);
+        }
+        this.info.value = `(Node: ${this.activeNode.id}) Move ${this.activeNode.moveNumber}:\n${JSON.stringify(this.activeNode.props)}`;
+    }
+
+    skipForeward() {
+        this.activeNode = this.getLastMainNode(this.activeNode);
+        console.log(this.activeNode);
+        this.boardState.innerText = this.prettify(this.activeNode.state);
+        this.info.value = `(Node: ${this.activeNode.id}) Move ${this.activeNode.moveNumber}:\n${JSON.stringify(this.activeNode.props)}`;
+    }
+}
+
+//need to define getNodeById() and prettify(); and gameTree; get ride of getState() since this should only reference a fully-formed node tree
+
+
+export default KosumiGoban;
---
+++ main.js
@@ -0,0 +1,65 @@
+import TextEditor from './textEditor/text-editor.js';
+import KosumiGoban from './goban/goban.js';
+import ParseSGF from './sgfStuff/parse-sgf.js';
+import MakeSGF from './sgfStuff/make-sgf.js';
+import {formatProps,getNodeById,getLastMainNode} from './sgfStuff/sgf-utils.js';
+import {initBoard,prettify,initStates,getState} from './sgfStuff/game-logic.js';
+
+const texteditor =  new TextEditor(document.getElementById('editorParent'));
+const goban = new KosumiGoban(document.getElementById('gobanParent'));
+
+goban.activeNode;
+goban.gameTree;
+goban.prettify = prettify;
+goban.getNodeById = getNodeById;
+goban.getState = getState;
+goban.getLastMainNode = getLastMainNode;
+
+let oldSGF;
+let newSGF;
+
+
+const toggleSGF = () => {
+    let toggleButton = document.getElementById('toggleButton');
+    if (toggleButton.innerText === 'show old SGF') {
+        toggleButton.innerText = 'show new SGF';
+        texteditor.textarea.value = oldSGF;
+    } else {
+        toggleButton.innerText = 'show old SGF';
+        texteditor.textarea.value = newSGF;
+    }
+}
+
+const lezgooo = () => {
+    oldSGF = texteditor.textarea.value;
+    goban.boardState;
+    let headBreak = true;
+    let nodeBreak = true;
+    goban.info;
+
+    goban.gameTree = formatProps(ParseSGF(oldSGF)[0]);
+
+    const EMPTY = initBoard(goban.gameTree);
+    goban.gameTree = initStates(EMPTY,goban.gameTree);
+
+    goban.activeNode = goban.gameTree;
+    goban.boardState.innerText = prettify(goban.activeNode.state);
+
+    newSGF = MakeSGF(goban.gameTree, headBreak, nodeBreak);
+    texteditor.textarea.value = newSGF;
+    texteditor.update();
+
+    goban.info.value = `(node ${goban.activeNode.id}) Move ${goban.activeNode.moveNumber}:\n${JSON.stringify(goban.activeNode.props)}`;
+
+    let toggleButton = document.getElementById('toggleButton');
+    if (!toggleButton) {
+        toggleButton = document.createElement('button');
+        toggleButton.id = 'toggleButton';
+        toggleButton.addEventListener('click', toggleSGF);
+        document.getElementById('settings').appendChild(toggleButton);
+    }
+    toggleButton.innerText = 'show old SGF';
+
+}
+
+document.getElementById('parse').addEventListener('click',lezgooo);
