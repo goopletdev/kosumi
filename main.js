d0f0218c85f1923ff300f9d5021842bd3f3c2d75
gooplet0075 <edmbeech@gmail.com>
2024-11-16 02:21:10 -0500

    put object creation in DOMContentLoaded listener... probably unnecessary

--- main.js
+++ main.js
@@ -3,27 +3,29 @@ import GobanCanvas from './goban/goban.js';
 import NavPanel from './navigation/navigation-panel.js';
 import StoneWalker from './stoneWalker/stone-walker.js';
 
-const texteditor =  new TextEditor(document.getElementById('editorParent'));
-const goban = new GobanCanvas(document.getElementById('gobanParent'));
-const navigator = new NavPanel(document.getElementById('navigationParent'));
-const walker = new StoneWalker();
+document.addEventListener('DOMContentLoaded',() => {
+    const texteditor =  new TextEditor(document.getElementById('editorParent'));
+    const navigator = new NavPanel(document.getElementById('navigationParent'));
+    const goban = new GobanCanvas(document.getElementById('gobanParent'));
+    const walker = new StoneWalker();
+    
+    // connect display and StoneWalker objects
+    walker.drive(goban, navigator);
+    navigator.walker = walker;
+    texteditor.walker = walker;
 
-// connect display and StoneWalker objects
-walker.drive(goban, navigator);
-navigator.walker = walker;
-texteditor.walker = walker;
-
-// splitBar resizer
-document.getElementById('splitBar').addEventListener('mousedown', () => {
-    function splitBarMove(cursor) {
-        let barStyle = splitBar.getBoundingClientRect();
-        let barWidth = barStyle.right-barStyle.left;
-        let leftEditorBounds = texteditor.parent.getBoundingClientRect().left;
-        let width = cursor.clientX - leftEditorBounds - (barWidth/2)
-        texteditor.parent.style.width = `${width}px`
-    }
-    document.addEventListener('mousemove', splitBarMove);
-    document.addEventListener('mouseup', () => {
-        document.removeEventListener('mousemove',splitBarMove)
-    })
-});
+    // splitBar resizer
+    document.getElementById('splitBar').addEventListener('mousedown', () => {
+        function splitBarMove(cursor) {
+            let barStyle = splitBar.getBoundingClientRect();
+            let barWidth = barStyle.right-barStyle.left;
+            let leftEditorBounds = texteditor.parent.getBoundingClientRect().left;
+            let width = cursor.clientX - leftEditorBounds - (barWidth/2)
+            texteditor.parent.style.width = `${width}px`
+        }
+        document.addEventListener('mousemove', splitBarMove);
+        document.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove',splitBarMove)
+        })
+    });
+})
