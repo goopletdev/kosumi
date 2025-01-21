132d2bc7ee7419c8d9524bae4077e101e6174d66
gooplet0075 <79529592+gooplet0075@users.noreply.github.com>
2025-01-21 07:01:48 -0500

    added janky testing stuff i guess

---
+++ test/sampleSGFs.js
@@ -0,0 +1,36 @@
+const rawSgfs = [
+    `(;GM[1]FF[4]CA[UTF-8]AP[CGoban:3]ST[2]
+RU[Japanese]SZ[9]KM[0.00]
+PW[White]PB[Black]
+;B[ee]
+;W[eg]
+;B[ec]
+(;W[gf]
+;B[cf]
+;W[gd]
+;B[dh]
+;W[gb]
+;B[fb]
+;W[eh]
+(;B[ei]
+;W[fi]
+;B[di]
+;W[dg]
+;B[fh])
+(;B[dg]
+;W[di]
+;B[ci]
+;W[ei]
+;B[ch]
+;W[ef])
+(;B[gc]
+;W[hc]))
+(;W[eb]
+;B[fb]
+;W[db]
+;B[fd]
+;W[cd]))
+`,
+];
+
+export { rawSgfs };
