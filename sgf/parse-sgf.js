d042ab9b08e15f4952a755c3ec30d34fcdb2e3bf
gooplet0075 <79529592+gooplet0075@users.noreply.github.com>
2025-01-21 07:03:16 -0500

    rewrote parsing methods to be easier to read and probably faster

--- sgf/parse-sgf.js
+++ sgf/parse-sgf.js
@@ -3,86 +3,53 @@
  * @module parse-sgf
  */
 
-import SGF from "./sgf.js";
-import { propertyDefinitions } from "./sgfProperties.js";
+import { propertyDefinitions } from './sgfProperties.js';
 
 /**
  * @private
  * @param {string} sgf SGF string to parse
  * @returns {{
- * type: (
- * '('|')'|';'|'propVal'|'propId');
- * value?: string;
- * depth?: number;
+ * type: ('terminal'|'propVal'|'propId');
+ * value: string;
  * }[]} array of token objects
  */
-function tokenize(sgf,callback) {
+const tokenize = async (sgf) => {
     let tokens = [];
 
-    let inpropId = false;
-    let propIdContent = '';
-
-    let bracketPos;
     let inBrackets = false;
     let escaped = false;
-    let bracketContents = '';
+    let value = '';
+
+    const pushToken = (type) => {
+        tokens.push({ type, value });
+        value = '';
+        if (type === 'propVal') inBrackets = false;
+    }
+
+    const handlePropVal = (char) => {
+        if (escaped) {
+            escaped = false;
+            if (char === ':') value += '<ESCAPEDCOLON>';
+            else if (char !== '\n') value += char;
+        } else if (char === '\\') escaped = true;
+        else if (char === ']') pushToken('propVal');
+        else value += char;
+    }
 
     for (let i = 0; i < sgf.length; i++) {
-
-        // handle square bracket contents (property value)
-        if (inBrackets && escaped) {
-            escaped = false;
-            bracketContents += sgf[i];
-        } else if (inBrackets && sgf[i] === '\\') {
-            escaped = true;
-        } else if (inBrackets && sgf[i] === ']') {
-            inBrackets = false;
-            tokens.push({
-                type: 'propVal',
-                value: bracketContents.slice(0)
-            })
-            bracketContents = '';
-        } else if (inBrackets && i < sgf.length - 1) {
-            bracketContents += sgf[i];
-        } else if (inBrackets) {
-            throw new Error(
-                `missing ']' after [ at ${bracketPos}`
-            )
-        } else if (sgf[i] === '[') {
-            bracketPos = i;
+        if (inBrackets) handlePropVal(sgf[i]);
+        else if (sgf[i] === '[') {
+            if (value) pushToken('propId');
             inBrackets = true;
-
-        // handle property identifier
         } else if (/[A-Z]/.test(sgf[i])) {
-            propIdContent += sgf[i];
-            inpropId = true;
-            if (!/[A-Z]/.test(sgf[i+1])) {
-                if (sgf[i+1] === '[') {
-                    inpropId = false;
-                }
-                tokens.push({
-                    type: 'propId',
-                    value: propIdContent.slice(0)
-                })
-                propIdContent = '';
-            }
-        } else if (inpropId) {
-            throw new Error(
-                `expecting propVal after propId '${
-                    tokens[tokens.length-1].value
-                }'`
-            )
-
-        // non-bracket terminal symbols
-        } else if ('();'.includes(sgf[i])) {
-            tokens.push({
-                type: sgf[i]
-            })
-        } else {
-            // console.log(`'${sgf[i]}' not valid SGF char`);
-        }
+            value += sgf[i];
+        } else if (value) throw new Error('expecting propVal after propId');
+        else if ('();'.includes(sgf[i])) tokens.push({ type: 'terminal', value: sgf[i] });
     }
-    callback(tokens);
+
+    if (inBrackets) throw new Error("missing ']'");
+
+    return tokens;
 }
 
 /**
@@ -94,162 +61,77 @@ function tokenize(sgf,callback) {
  * id?: number;
  * value?: string;
  * props?: {};
- * }[]} Condensed token list
+ * }[]} Condensed token array
  */
-function parseTokens(tokens,callback) {
-
-    let parsedToks = [];
-    let inNode = false;
-    let propertyId = '';
+const parseTokens = async (tokens) => {
+    let nodes = [];
+    let node = null;
+    let propId = '';
     let id = 0;
 
-    for (let i=0; i<tokens.length;i++) {
+    const handleTerminal = (char) => {
+        if ('()'.includes(char)) node &&= null;
+        else node = { id: id++ };
+        nodes.push( node || char );
+    }
 
-        let tok = tokens[i]
+    const handlePropId = (id) => {
+        propId = id;
+        if (node.props?.[propId]) throw new Error('duplicate property');
+        if (!node.props) node.props = {};
+        node.props[propId] = [];
+    }
 
-        // open/close game tree tokens
-        if ('()'.includes(tok.type)) {
-            parsedToks.push(tok);
-
-        // new node token
-        } else if (tok.type === ';') {
-            if (tokens[i+1].type === 'propId') {
-                inNode = true;
-            } else {
-                console.log('empty node');
-            }
-            tok.id = id;
-            id ++;
-            parsedToks.push(tok);
-
-        // handle node properties
-        } else if (inNode) {
-            let node = parsedToks[parsedToks.length-1];
-            if (!node.hasOwnProperty('props')){
-                node.props = {};
-            }
-            if (tok.type === 'propId') {
-                propertyId = tok.value;
-                if (node.props.hasOwnProperty(propertyId)) {
-                    // duplicate property
-                    console.log(`Node ${node.id} already includes ${propertyId}`)
-                } else {
-                    node.props[propertyId] = [];
-                }
-                if (tokens[i+1].type !== 'propVal') {
-                    throw new Error(
-                        `expected propVal after ${
-                            propertyId
-                        }`
-                    )
-                }
-            } else { // token.type is propVal
-                // unzip coordinates, convert alpha to numeric coords
-                if (SGF.zippableProperties.includes(propertyId)
-                    && tok.value.includes(':')) {
-                        for (let value of SGF.unzipCoords(tok.value)) {
-                            node.props[propertyId].push(SGF.numericCoord(value));
-                        }
-                } else if (propertyDefinitions.hasOwnProperty(propertyId)) {
-                    // otherwise this is a private property
-                    if (propertyDefinitions[propertyId].value.includes('stone')
-                        || propertyDefinitions[propertyId].value.includes('point')
-                        || ['B','W'].includes(propertyId)) {
-                            node.props[propertyId].push(SGF.numericCoord(tok.value));
-                    } else {
-                        node.props[propertyId].push(tok.value);
-                    }
-                } else {
-                    // private property
-                    console.log('private property:',propertyId,tok.value);
-                    node.props[propertyId].push(tok.value);
-                }
-                if ('();'.includes(tokens[i+1].type)) {
-                    inNode = false;
-                }
-            }
-
+    const handlePropVal = (propVal) => {
+        const def = propertyDefinitions[propId];
+        if (def) node.props[propId].push(...def.parse(propVal));
+        else {
+            console.log('private property');
+            node.props[propId].push(propVal);
         }
     }
-    callback(parsedToks);
-}
 
-/**
- * @private
- * @param {Array} toks Slice of toks starting w/ '('
- * @returns {number} Position of matching ')' token
- */
-function getTreeEnd(toks) {
-    let j = -1;
-    for (let i = 1; i < toks.length; i++) {
-        let token = toks[i];
-        if (token.type === '(') {
-            j++;
-        } else if (token.type === ')' && j < 0) {
-            return i;
-        } else if (token.type === ')') {
-            j--;
-        }
+    for (let i=0; i < tokens.length; i++) {
+
+        const tok = tokens[i];
+
+        if (tok.type === 'terminal') handleTerminal(tok.value);
+        else if (!node) throw new Error(`cannot assign ${tok} to node ${node}`);
+        else if (tok.type === 'propId') handlePropId(tok.value);
+        else if (tok.type === 'propVal') handlePropVal(tok.value);
     }
-    return -1;
+
+    return nodes;
 }
 
-/**
- * @private
- * @param {Array} tokens Tokens of type '(', ')', ';'
- * @returns {{}} Node tree of all moves and variations
- */
-function makeTree(tokens, parent, move = 0) {
-    if (tokens) {
-        if (tokens[0].type === '(') {
-            let trees = [];
-            while (tokens.length) {
-                let treeEnd = getTreeEnd(tokens);
-                let subTree = tokens.slice(1,treeEnd);
-                let subNode = makeTree(
-                    subTree, parent, move
-                );
-                trees.push(subNode);
-                tokens = tokens.slice(treeEnd+1);
+const buildGameObject = async (tokens) => {
+    let position = 0;
+    let depth = 0;
+    
+    const makeTree = (parent = null, moveNumber = 0) => {
+        const tok = tokens[position++];
+
+        if (tok === ')') {
+            depth--;
+            return null;
+        } else if (tok === '(') {
+            const surface = depth++;
+            const branches = [];
+            while (position < tokens.length) {
+                const branch = makeTree(parent,moveNumber);
+                if (branch) branches.push(...branch);
+                if (depth < surface) break;
             }
-            return trees;
-        } else if (tokens[0].type === ';') {
-            let node = {
-                id: tokens[0].id,
-                moveNumber: move
-            }
-            if (parent) {
-                node.parent = parent;
-            }
-            if (tokens[0].hasOwnProperty('props')) {
-                node.props = tokens[0].props;
-                // check whether erroneous root props at non-root node
-                if (node.hasOwnProperty('parent')) {
-                    for (let key of Object.keys(node.props)) {
-                        if (SGF.rootProperties.includes(key)) {
-                            console.log(`Error: root ${key} at node ${node.id}`);
-                        }
-                    }
-                }
-                
-            }
-            if (tokens.length > 1) {
-                let childs = makeTree(
-                    tokens.slice(1),node,move+1
-                );
-                if (Array.isArray(childs)) {
-                    node.children = childs;
-                } else {
-                    node.children = [childs];
-                }
-            }
-            return node;
+            return branches;
         }
-    } else {
-        throw new Error(
-            `'tokens' in makeTree is ${tokens}`
-        )
+        const node = { ...tok, moveNumber, parent };
+        node.children = makeTree(node, moveNumber+1) || [];
+        return [node];
     }
+
+    return makeTree();
 }
 
-export {tokenize, parseTokens, makeTree};
+
+
+export { tokenize, parseTokens, buildGameObject };
