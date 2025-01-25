0193ed2230c79484a82df3fc3e10c1d0fc2d252d
gooplet0075 <edmbeech@gmail.com>
2025-01-24 23:42:57 -0500

    added a couple more tests

--- sgf/sgfUtils.test.js
+++ sgf/sgfUtils.test.js
@@ -1,7 +1,21 @@
-import { numCoord } from "./sgfUtils.js"
+import { parseCoord, encodeCoord, unzipCoords } from "./sgfUtils.js"
 
-test("numCoord", () => {
-    expect(numCoord("aa")).toEqual([0, 0]);
-    expect(numCoord("AA")).toEqual([26, 26]);
-    expect(numCoord("tt")).toEqual([19, 19]);
+describe("SGF utility functions", () => {
+    test("parseCoord", () => {
+        expect(parseCoord("aa")).toEqual([0, 0]);
+        expect(parseCoord("AA")).toEqual([26, 26]);
+        expect(parseCoord("tt")).toEqual([19, 19]);
+    });
+
+    test("encodeCoord", () => {
+        expect(encodeCoord([1,2])).toEqual('bc');
+        expect(encodeCoord([18,18])).toEqual('ss');
+        expect(encodeCoord([0,5])).toEqual('af');
+    });
+
+    test("unzipCoords", () => {
+        expect(unzipCoords('ab:bc')).toEqual([[0,1],[0,2],[1,1],[1,2]]);
+        expect(unzipCoords('aa:bb')).toEqual([[0,0],[0,1],[1,0],[1,1]]);
+        expect(unzipCoords('bb:bd')).toEqual([[1,1],[1,2],[1,3]]);
+    });
 });
