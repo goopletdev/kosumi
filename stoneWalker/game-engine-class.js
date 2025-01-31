98ca435dec6f5c0f0225f018dc51705537317381
gooplet0075 <edmbeech@gmail.com>
2025-01-31 05:32:11 -0500

    reimplemented move function under Goban game engine class

--- stoneWalker/game-engine-class.js
+++ stoneWalker/game-engine-class.js
@@ -73,18 +73,37 @@ class Goban {
      * @returns {Array.<number>} All the stones in a chain of stones
      */
     getChain (fCoord) {
-        const chain=[];
+        const coords=[];
 
         const search = (coord) => {
-            chain.push(coord);
+            coords.push(coord);
             this.links(coord).forEach(link => {
-                if (!chain.includes(link)) search(link);
+                if (!coords.includes(link)) search(link);
             });
         }
 
         if (this.state[fCoord]) search(fCoord);
 
-        return chain;
+        return coords;
+    }
+
+    /**
+     * Find chain's unoccupied adjacent intersections 
+     * @param {number} fCoord Flattened coordinate
+     * @returns {Array.<number>} Chain's liberties
+     */
+    getLiberties (fCoord) {
+        const dame = []; // array of chain's liberties
+        if (!this.state[fCoord]) return dame;
+        
+        this.getChain(fCoord).forEach(link => {
+            this.neighbors(link).forEach(neighbor => {
+                if (dame.includes(neighbor) || this.state[neighbor]) return;
+                dame.push(neighbor);
+            });
+        });
+
+        return dame;
     }
 
     /**
@@ -96,6 +115,61 @@ class Goban {
     setup (value, ...fCoords) {
         fCoords.forEach(coord => this.state[coord] = value);
         return this.state;
+    }
+
+    /**
+     * Mutates game state by placing stones, then removes captures.
+     * Allows for suicide moves, and notably for multiple simultaneous
+     * moves (for variants).
+     * @param {1 | 2} value Color/player number
+     * @param  {...number} fCoords 
+     * @returns {(Array.<number> | undefined)[]} captured stones
+     * @throws on an attempt to place a stone on an occupied intersection
+     */
+    move (value, ...fCoords) {
+        // validate and place moves
+        fCoords.forEach(c => {
+            if (this.state[c]) throw new Error(`Intersection ${c} is already occupied`);
+            this.state[c] = value;
+        });
+
+        // track captures by color
+        const captures = [,];
+
+        // check for opponent captures
+        fCoords.forEach(move => {
+            this.neighbors(move).forEach(neighbor => {
+                const color = this.state[neighbor];
+                if (!color || color === value) return;
+                if (this.getLiberties(neighbor).length) return;
+                if (!captures[color]) {
+                    captures[color] = new Set();
+                } else if (captures[color].has(neighbor)) return;
+                
+                this.getChain(neighbor).forEach(link => {
+                    console.log(link);
+                    captures[color].add(link);
+                });
+            });
+        });
+
+        // remove captures from board
+        captures.forEach(color => {
+            if (!color) return;
+            color.forEach(stone => this.state[stone] = 0);
+        });
+
+        // check for self-captured stones
+        fCoords.forEach(stone => {
+            if (this.getLiberties(stone).length) return;
+            if (!captures[value]) captures[value] = new Set();
+            this.getChain(stone).forEach(link => captures[value].add(link));
+        });
+
+        // remove self-captures from board
+        captures[value]?.forEach(stone => this.state[stone] = 0);
+
+        return captures;
     }
 
     /**
@@ -112,7 +186,7 @@ class Goban {
     }
 
     /**
-     * Sets given state 
+     * Sets given state to new Goban object and returns Goban
      * @param {Array.<number>} state Flat square array
      * @returns {Goban} Goban object with the given state
      */
